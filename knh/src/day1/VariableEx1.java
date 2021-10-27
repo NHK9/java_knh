7b2fce75a4561c093629d7fd54e4aeb81f5aa8b1
NHK9 <skagud3217@gmail.com>
2021-10-27 16:49:24 +0900

    1-2 변수 선언 예제

---
+++ knh/src/day1/VariableEx1.java
@@ -0,0 +1,30 @@
+package day1;
+
+public class VariableEx1 {
+
+	public static void main(String[] args) {
+		// 한줄 주석
+		/* 여러줄
+		 * 주석
+		 */
+		char score1;
+		char score2 = 'F';
+		//나이를 관리하는 변수를 선언해보세요
+		int age1;
+		int age2 = 1;
+		//성적 평균 관리하는 변수 선언 단, 성적은 0~100점까지
+		double avg1;
+		double avg2 = 0.0;
+		float avg3 = 0.0f;
+		
+		//변수 age1을 이용해서 성인인지 아닌지 판별하는 변수 선언
+		boolean isAdult1;
+		boolean isAdult2 = true;//true,false
+		
+		int num1 = 1000;
+		
+		score1 = 'A';
+		//System.out.println(isAdult1);// 변수 초기화안해서 에러발생
+	}
+
+}
